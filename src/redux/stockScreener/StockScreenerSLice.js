b880ae6722da9ffdbee24e994cf0ea14f06a500b
user-faiz <email2faizm@gmail.com>
2023-09-28 15:08:39 +0530

    update SctockScreenerSlice.js

--- src/redux/stockScreener/StockScreenerSLice.js
+++ src/redux/stockScreener/StockScreenerSLice.js
@@ -1,10 +1,11 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const fetchStockScreenerData = createAsyncThunk(
   'stockScreeners/fetchStockScreener',
   async () => {
     try {
-      const response = await fetch('https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&sector=Technology&exchange=NASDAQ&dividendMoreThan=0&limit=100&apikey=75923f68a3b79acd5d5f2a07307c25b2');
+      const response = await fetch('https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&sector=Technology&exchange=NASDAQ&dividendMoreThan=0&limit=100&apikey=a97307caefae90fd49ae8c8e45d6bbbf');
       const data = await response.json();
       return data;
     } catch (error) {
@@ -12,7 +13,6 @@ export const fetchStockScreenerData = createAsyncThunk(
     }
   },
 );
-fetchStockScreenerData();
 
 const stockScreenersSlice = createSlice({
   name: 'stockScreeners',
