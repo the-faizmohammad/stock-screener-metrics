d2054a4796c40c4ab04c2e66b391f7dfefad0948
user-faiz <email2faizm@gmail.com>
2023-09-28 16:46:27 +0530

    Update Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => (
+    <footer className="footer-name">
+      &copy; 2023 Stock Screener - Developed by
+       Faiz Mohammad.
+    </footer>
+  );
+  
+  export default Footer;
