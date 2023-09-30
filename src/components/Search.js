05c18e474fe71d3b2892a3d9737b0a7d3a1c8983
user-faiz <email2faizm@gmail.com>
2023-10-01 02:20:17 +0530

    update package.json

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,13 @@
 import { useDispatch } from 'react-redux';
-import debounce from 'lodash/debounce';
 import { searchCompany } from '../redux/stockScreener/StockScreenersSlice';
 import './styles/Search.css';
 
 function Search() {
   const dispatch = useDispatch();
 
-  const debouncedSearch = debounce((searchValue) => {
-    dispatch(searchCompany(searchValue.toLowerCase()));
-  }, 300);
-
   const searchQuery = (e) => {
     const searchValue = e.target.value;
-    debouncedSearch(searchValue);
+    dispatch(searchCompany(searchValue.toLowerCase()));
   };
 
   return (
